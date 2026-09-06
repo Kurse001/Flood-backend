360e48c674bedbdb3dd5a1ea10cde2005b21828e
Kurse001 <105558553+Kurse001@users.noreply.github.com>
2026-09-06 14:32:01 +0530

    Add CORS middleware
    Added CORS middleware to allow requests from specified origins.

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "https://flood-app-geoloc.vercel.app",
+    ],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/risk-zones")
 def get_risk_zones(lat: float, lng: float):
