8f3e2494f0faf74258ebf4a965b2cf414e0c0254
Kurse001 <105558553+Kurse001@users.noreply.github.com>
2026-09-08 13:42:09 +0530

    Fix typo in file path for gully inlets dataset

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ with open("data/manholes.geojson") as f:
 
 # --- Load elevation-enriched datasets directly from GeoPackage ---
 manholes_gdf = gpd.read_file("data/kmc_manholes_with_elevation.gpkg")
-gully_gdf = gpd.read_file("data/kmc_gully_inlets_with elevation.gpkkg")
+gully_gdf = gpd.read_file("data/kmc_gully_inlets_with elevation.gpkg")
 drainage_elevation_gdf = gpd.readfile("data/drainage_with_elevation.gpkg")
 
 def haversine(lat1, lng1, lat2, lng2):
